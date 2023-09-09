12bfe6d2ef68712dd143a2c8881980ff8a2613b2
mkbhru <mkbhru@gmail.com>
2023-09-09 17:28:24 +0530

    till carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import PreNavbar from './components/PreNavbar';
+import Slider from './components/Slider';
+import data from "./data/data.json"
 
 function App() {
   return (
     <div>
       <PreNavbar />
       <Navbar />
+      <Slider start = {data.banner.start}/>
     </div>
   );
 }
